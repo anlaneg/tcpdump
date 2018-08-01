70a6f4f1190d5fd0290b00f4854e14384efdf2b0
anlaneg <anlaneg@126.com>
2018-08-01 11:58:43 +0800

    add do.sh script

---
+++ do.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+function abort_file_not_exist()
+{
+	dir="$1"
+	msg="$2"
+
+	if [ ! -e "$dir" ];
+	then
+		echo "$msg"
+		exit 1
+	fi;
+
+}
+
+function abort_dir_not_exist()
+{
+	dir="$1"
+	msg="$2"
+	if [ ! -d "$dir" ];
+	then
+		echo "$msg"
+		exit 1
+	fi;
+}
+
+#echo "Usage: (cd .. ; ln -s anlaneg_tcpdump tcpdump ; ln -s anlaneg_libpcap libpcap)"
+abort_dir_not_exist "../tcpdump" "need 'tcpdump' directory"
+abort_dir_not_exist "../libpcap" "need 'libpcap' directory"
+
+abort_file_not_exist "../tcpdump/do.sh" "need tcpdump/do.sh file"
+
+diff_str=`diff -Nu ../tcpdump/do.sh $0`
+if [ "X$diff_str" == "X" ];
+then
+	(echo "do build libpcap" ; cd ../libpcap ; ./configure ; make ;)
+	(echo "do build tcpdump" ; cd ../tcpdump ; ./configure ; make ;)
+fi;
